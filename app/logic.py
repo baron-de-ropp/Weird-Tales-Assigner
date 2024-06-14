1ba23364435175d56c9782a843671a560a345f55
Ryan de Ropp <daudzegir@gmail.com>
2024-06-14 17:27:18 -0400

    version 1 complete!

--- app/logic.py
+++ app/logic.py
@@ -8,24 +8,23 @@ def get_random_images(app_root):
         image_list = json.load(f)
     return random.sample(image_list, 3)
 
-def check_existing_assignment(email):
+def check_existing_assignment(itch_username):
     if not os.path.exists('assignments.csv'):
         return None
     with open('assignments.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if row['email'] == email:
+            if row['itch_username'] == itch_username:
                 return {
-                    'email': row['email'],
                     'itch_username': row['itch_username'],
                     'images': json.loads(row['images'])
                 }
     return None
 
-def log_assignment(email, itch_username, images):
+def log_assignment(itch_username, images):
     with open('assignments.csv', 'a', newline='') as csvfile:
-        fieldnames = ['email', 'itch_username', 'images']
+        fieldnames = ['itch_username', 'images']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if csvfile.tell() == 0:
             writer.writeheader()
-        writer.writerow({'email': email, 'itch_username': itch_username, 'images': json.dumps(images)})
+        writer.writerow({'itch_username': itch_username, 'images': json.dumps(images)})
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 import random
-from app.email_utils import send_email
 from app.logic import get_random_images, check_existing_assignment, log_assignment
 import os
 
@@ -13,17 +12,16 @@ def index():
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
     if request.method == 'POST':
-        email = request.form['email']
         itch_username = request.form['itch_username']
-        existing_assignment = check_existing_assignment(email)
+        existing_assignment = check_existing_assignment(itch_username)
         if existing_assignment:
             images = existing_assignment['images']
             message = "These were your assigned images!"
         else:
             images = get_random_images(app.root_path)
-            log_assignment(email, itch_username, images)
-            send_email(email, images)
+            log_assignment(itch_username, images)
             message = "Your inspiration challenge prompts are..."
 
-        return render_template('assignment.html', images=images, email=email, itch_username=itch_username, message=message)
+        return render_template('assignment.html', images=images, itch_username=itch_username, message=message)
     return render_template('submit.html')
+
